e3c48c34cf228a82ec8dc33885119f5a036b657f
Hisham Dulfacker <dulfackerhisham@gmail.com>
2023-11-15 12:00:06 +0530

    function working

---
+++ 01_basics/01variables.js
@@ -0,0 +1,25 @@
+//  var fullName = "Hisham Dulfacker"; 
+
+//  var phone;
+//  console.log(phone)
+
+// //  console.log(toy) => undeclared
+
+// let num = 100.94;
+// let nums = 100;
+// console.log(typeof num)
+
+
+fullName()
+console.log(x)
+console.log(fullName)
+
+
+
+var x = 5
+
+function fullName(){
+    console.log("Hisham Dulfacker")
+}
+
+
---
+++ 01_basics/functionWorking.js
@@ -0,0 +1,15 @@
+var y = 1;
+a();
+b();
+console.log(y)
+
+
+function a(){
+    var y = 10;
+    console.log(y)
+}
+
+function b(){
+    var y = 104;
+    console.log(y)
+}
