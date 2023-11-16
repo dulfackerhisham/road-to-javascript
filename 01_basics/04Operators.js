1220a08b284dd6cfb4cf01e856f54979abcc1c38
Hisham Dulfacker <dulfackerhisham@gmail.com>
2023-11-16 10:59:14 +0530

    operators and type of

---
+++ 01_basics/04Operators.js
@@ -0,0 +1,24 @@
+// + - * / %
+
+var num1 = 6;
+var num2 = 5
+
+console.log(num1 + num2)
+
+var answer = num1 < num2;
+console.log(answer)
+
+console.log("**************************************************")
+var sellingPrice = 199;
+var listingPrice = 799;
+
+var discountPercent = ((listingPrice - sellingPrice) / listingPrice) * 100;
+
+console.log("Discount percentage is " + discountPercent)
+displayDiscounrPercentage = Math.round(discountPercent)
+
+console.log("Discount percentage is " + displayDiscounrPercentage)
+
+var result = listingPrice > sellingPrice
+
+console.log(typeof result)
